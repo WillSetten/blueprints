4672db6e182cbad3e8a1909dac9c89effc5c2718
Will Setten <willsetten@hotmail.co.uk>
2019-06-17 16:04:56 +0100

    More Pathfinding work
    Created new TileMap methods for assisting pathfinding

--- Assets/Scripts/Tile Scripts/TileMap.cs
+++ Assets/Scripts/Tile Scripts/TileMap.cs
@@ -9,13 +9,20 @@ public class TileMap : MonoBehaviour
     public TileType[] tileTypes;
 
     int[,] tiles; //2D Integer array for showing which tiles are passable and which aren't
+    Node[,] graph; //2D Array of Nodes for pathfinding
 
     int mapSizeX = 10;
     int mapSizeY = 10;
 
     private void Start()
     {
+        //Setup the selected units variables
+        selectedUnit.GetComponent<Unit>().tileX = (int)selectedUnit.transform.position.x;
+        selectedUnit.GetComponent<Unit>().tileY = (int)selectedUnit.transform.position.y;
+        selectedUnit.GetComponent<Unit>().map = this;
+
         GenerateMapData();
+        GeneratePathfindingGraph();
         GenerateMapVisuals();
     }
 
@@ -26,11 +33,11 @@ public class TileMap : MonoBehaviour
         tiles = new int[mapSizeX, mapSizeY];
 
         //Initialize map
-        for (int i = 0; i < mapSizeX; i++)
+        for (int x= 0; x< mapSizeX; x++)
         {
-            for (int j = 0; j < mapSizeY; j++)
+            for (int y = 0; y < mapSizeY; y++)
             {
-                tiles[i, j] = 0;
+                tiles[x, y] = 0;
             }
         }
         tiles[3, 4] = 1;
@@ -46,36 +53,207 @@ public class TileMap : MonoBehaviour
     //Generates the actual tiles from the given map
     void GenerateMapVisuals()
     {
-        for (int i = 0; i < mapSizeX; i++)
+        for (int x= 0; x< mapSizeX; x++)
         {
-            for (int j = 0; j < mapSizeY; j++)
+            for (int y = 0; y < mapSizeY; y++)
             {
-                TileType tt = tileTypes[tiles[i, j]];
-                GameObject go = (GameObject)Instantiate(tt.tileVisualPrefab, new Vector3(i, j, 0), Quaternion.identity);
+                TileType tt = tileTypes[tiles[x , y]];
+                GameObject go = (GameObject)Instantiate(tt.tileVisualPrefab, new Vector3(x, y, 0), Quaternion.identity);
 
                 ClickableTile ct = go.GetComponent<ClickableTile>();
-                ct.tileX = i;
-                ct.tileY = j;
+                ct.tileX = x ;
+                ct.tileY = y;
                 ct.map = this;
             }
         }
     }
 
-    //Converts Tile Co-ordinates with Co-ordinates in the actual world. For now, this is just a matter of converting two ints into
+    //Converts Tile Co-ordinates with Co-ordinates in the actual world. For now, this is yust a matter of converting two ints into
     //a Vector3. Any scaling issues will probably have to be fixed in this method.
     public Vector3 TileCoordToWorldCoord(int x, int y)
     {
         return new Vector3(x, y, 0);
     }
 
-    //Takes in an x and y to move the selected unit to.
-    public void MoveSelectedUnitTo(int x, int y)
+    //Takes in an x and y to move the selected unit to. This method currently uses basic Dyikstra
+    public void GeneratePathTo(int x, int y)
     {
-        //Update the units information so that it knows where it is.
-        selectedUnit.GetComponent<Unit>().tileX = x;
-        selectedUnit.GetComponent<Unit>().tileY = y;
-        //Move the position of the unit.
-        selectedUnit.transform.position = TileCoordToWorldCoord(x, y);
+        // Clear out our unit's old path.
+        selectedUnit.GetComponent<Unit>().currentPath = null;
+
+        Dictionary<Node, float> dist = new Dictionary<Node, float>();
+        Dictionary<Node, Node> prev = new Dictionary<Node, Node>();
+
+        // Setup the "Q" -- the list of nodes we haven't checked yet.
+        List<Node> unvisited = new List<Node>();
+
+        Node source = graph[
+                            selectedUnit.GetComponent<Unit>().tileX,
+                            selectedUnit.GetComponent<Unit>().tileY
+                            ];
+
+        Node target = graph[
+                            x,
+                            y
+                            ];
+
+        dist[source] = 0;
+        prev[source] = null;
+
+        // Initialize everything to have INFINITY distance, since
+        // we don't know any better right now. Also, it's possible
+        // that some nodes CAN'T be reached from the source,
+        // which would make INFINITY a reasonable value
+        foreach (Node v in graph)
+        {
+            if (v != source)
+            {
+                dist[v] = Mathf.Infinity;
+                prev[v] = null;
+            }
+
+            unvisited.Add(v);
+        }
+
+        while (unvisited.Count > 0)
+        {
+            // "u" is going to be the unvisited node with the smallest distance.
+            Node u = null;
+
+            foreach (Node possibleU in unvisited)
+            {
+                if (u == null || dist[possibleU] < dist[u])
+                {
+                    u = possibleU;
+                }
+            }
+
+            if (u == target)
+            {
+                break;  // Exit the while loop!
+            }
+
+            unvisited.Remove(u);
+
+            foreach (Node v in u.neighbours)
+            {
+                //float alt = dist[u] + u.DistanceTo(v);
+                float alt = dist[u] + CostToEnterTile(x,y,v.x,v.y);
+                if (alt < dist[v])
+                {
+                    dist[v] = alt;
+                    prev[v] = u;
+                }
+            }
+        }
+
+        // If we get there, the either we found the shortest route
+        // to our target, or there is no route at ALL to our target.
+
+        if (prev[target] == null)
+        {
+            // No route between our target and the source
+            return;
+        }
+
+        List<Node> currentPath = new List<Node>();
+
+        Node curr = target;
+
+        // Step through the "prev" chain and add it to our path
+        while (curr != null)
+        {
+            currentPath.Add(curr);
+            curr = prev[curr];
+        }
+
+        // Right now, currentPath describes a route from out target to our source
+        // So we need to invert it!
+
+        currentPath.Reverse();
+
+        selectedUnit.GetComponent<Unit>().currentPath = currentPath;
     }
 
+    //Generates a series of nodes from the graph which define which tiles are connected to which tiles.
+    public void GeneratePathfindingGraph()
+    {
+        // Initialize the array
+        graph = new Node[mapSizeX, mapSizeY];
+
+        // Initialize a Node for each spot in the array
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeX; y++)
+            {
+                graph[x, y] = new Node();
+                graph[x, y].x = x;
+                graph[x, y].y = y;
+            }
+        }
+
+        // Calculate neighbours
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeX; y++)
+            {
+                // Try left
+                if (x > 0)
+                {
+                    graph[x, y].neighbours.Add(graph[x - 1, y]);
+                    if (y > 0)
+                        graph[x, y].neighbours.Add(graph[x - 1, y - 1]);
+                    if (y < mapSizeY - 1)
+                        graph[x, y].neighbours.Add(graph[x - 1, y + 1]);
+                }
+
+                // Try Right
+                if (x < mapSizeX - 1)
+                {
+                    graph[x, y].neighbours.Add(graph[x + 1, y]);
+                    if (y > 0)
+                        graph[x, y].neighbours.Add(graph[x + 1, y - 1]);
+                    if (y < mapSizeY - 1)
+                        graph[x, y].neighbours.Add(graph[x + 1, y + 1]);
+                }
+
+                // Try straight up and down
+                if (y > 0)
+                    graph[x, y].neighbours.Add(graph[x, y - 1]);
+                if (y < mapSizeY - 1)
+                    graph[x, y].neighbours.Add(graph[x, y + 1]);
+            }
+        }
+    }
+
+    //Determines the cost to enter a tile in position x,y
+    public float CostToEnterTile(int sourceX, int sourceY, int targetX, int targetY)
+    {
+
+        TileType tt = tileTypes[tiles[targetX, targetY]];
+
+        if (UnitCanEnterTile(targetX, targetY) == false)
+            return Mathf.Infinity;
+
+        float cost = tt.movementCost;
+
+        if (sourceX != targetX && sourceY != targetY)
+        {
+            // We are moving diagonally!  Fudge the cost for tie-breaking
+            // Purely a cosmetic thing!
+            cost += 0.001f;
+        }
+
+        return cost;
+
+    }
+
+    public bool UnitCanEnterTile(int x, int y)
+    {
+
+        // We could test the unit's walk/hover/fly type against various
+        // terrain flags here to see if they are allowed to enter the tile.
+
+        return tileTypes[tiles[x, y]].isWalkable;
+    }
 }
