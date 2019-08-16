567cf8309427150e74aa9f85b7f1332dabac2a03
Will Setten <willsetten@hotmail.co.uk>
2019-08-16 16:25:34 +0100

    Added in vault scripts and assets
    Added in vault scripts and assets. Vault currently opens when clicked.

---
+++ Assets/Assets/Scripts/Object Scripts/Vault.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vault : MonoBehaviour
+{
+    VaultDoor vaultDoor;
+    HingeJoint2D hinge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vaultDoor = GetComponentInChildren<VaultDoor>();
+        hinge = GetComponentInChildren<HingeJoint2D>();
+    }
+
+    private void OnMouseUp()
+    {
+        hinge.useMotor=true;
+    }
+}
--- Assets/Assets/Scripts/Tile Scripts/Tile.cs
+++ Assets/Assets/Scripts/Tile Scripts/Tile.cs
@@ -39,7 +39,7 @@ public class Tile : MonoBehaviour
     private void Update()
     {
         //if there is not a unit on this tile
-        if (Physics2D.OverlapCircleAll((Vector2)transform.position, 0.1f, LayerMask.GetMask("EnemyUnits", "PlayerUnits", "CivilianUnits", "Loot", "Vehicles")).Length==0)
+        if (Physics2D.OverlapCircleAll((Vector2)transform.position, 0.1f, LayerMask.GetMask("EnemyUnits", "PlayerUnits", "CivilianUnits", "Loot", "Vehicles", "Vault")).Length==0)
         {
             occupied = false;
             blocked = false;
@@ -47,19 +47,24 @@ public class Tile : MonoBehaviour
         //If there is a unit over this tile
         else
         {
-            Collider2D collider = Physics2D.OverlapCircleAll((Vector2)transform.position, 0.1f, LayerMask.GetMask("EnemyUnits", "PlayerUnits", "CivilianUnits", "Loot","Vehicles"))[0];
-            if (collider.GetComponent<Loot>()|| collider.GetComponent<Truck>())
+            Collider2D collider = Physics2D.OverlapCircleAll((Vector2)transform.position, 0.5f, LayerMask.GetMask("EnemyUnits", "PlayerUnits", "CivilianUnits", "Loot", "Vehicles", "Vault"))[0];
+            if (collider.GetComponent<Loot>()|| collider.GetComponent<Truck>()|| collider.GetComponent<VaultDoor>())
             {
                 occupied = true;
                 blocked = true;
             }
-            else if (collider.GetComponent<Rigidbody2D>().velocity == Vector2.zero) {
+            else if (collider.GetComponent<Unit>()&&collider.GetComponent<Rigidbody2D>().velocity == Vector2.zero) {
                 if (collider.GetComponent<Unit>().isLarge) {
                     blocked = true;
                 }
                 occupied = true;
                 isDestination = false;
                 GetComponent<SpriteRenderer>().color = Color.white;
+            }
+            else
+            {
+                occupied = false;
+                blocked = false;
             }
         }
     }
